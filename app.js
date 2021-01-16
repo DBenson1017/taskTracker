84b719f48b5af0be62e8802d8294032a64ef318e
DBenson1017 <devinbenson@gmail.com>
2021-01-15 21:50:18 -0500

    delete button bug fix

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ function deleteCheck(e){
     const item = e.target;
     //DELETE TODO
     if(item.classList[0] === 'trash-btn'){
-        item.remove();
+        const todo = item.parentElement;
+        todo.remove 
     } 
 }
